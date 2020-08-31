d635a516538a56ba13c153110aaac8f5656c46f9
azhry <arliansyah_azhary@yahoo.com>
2020-08-31 12:49:19 +0700

    bug fix nav.tsx

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 import Link from 'next/link'
 
 const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
+  { 
+    key: 'zeit',
+    href: 'https://zeit.co/now', 
+    label: 'ZEIT' 
+  },
+  { 
+    key: 'github',
+    href: 'https://github.com/zeit/next.js', 
+    label: 'GitHub' 
+  },
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`
   return link
